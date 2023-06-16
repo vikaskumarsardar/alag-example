56bd8fa12d4ff6902dba1b93552694eaeaf0cc7c
swapan <swapan@gmail.com>
2023-06-16 16:25:38 +0530

    yeh le

--- src/App.js
+++ src/App.js
@@ -6,27 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
-          }
-        />
+        <Route path="/" element={<div>This is a Div</div>} />
         <Route path="/home" element={<div>This is a Home Page</div>} />
         {/* <Route path=":ROOM_ID" element={<Video />} /> */}
       </Routes>
