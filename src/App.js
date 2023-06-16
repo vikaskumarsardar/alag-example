5510146a97ea0248d44a4d6472b0dc43f9f7970d
swapan <swapan@gmail.com>
2023-06-16 16:37:11 +0530

    lelelelele

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Routes, BrowserRouter, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>This is a Div</div>} />
-        <Route path="/home" element={<div>This is a Home Page</div>} />
-        {/* <Route path=":ROOM_ID" element={<Video />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
