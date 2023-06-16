3554064f1efdc5b9afcc523a37c18efaaacdd1ac
swapan <swapan@gmail.com>
2023-06-16 16:24:26 +0530

    adddd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, BrowserRouter, Route, Link } from "react-router-dom";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -23,7 +23,6 @@ function App() {
                 >
                   Learn React
                 </a>
-                {/* <Link to="/home">Click to Home</Link> */}
               </header>
             </div>
           }
